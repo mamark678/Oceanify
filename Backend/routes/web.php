01f20a3e7f721dab8810c34123390e87f822ca59
andremarc-droid <andremarcsambile@gmail.com>
2025-10-30 18:59:54 +0800

    Update web.php to serve React app

--- Backend/routes/web.php
+++ Backend/routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// API routes should be in routes/api.php
+// This catches all routes and serves the React app
+
+Route::get('/{any}', function () {
+    return file_get_contents(public_path('index.html'));
+})->where('any', '^(?!api).*$');
